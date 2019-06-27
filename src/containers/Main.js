c221c8bddbece590001f496388714aa68b2c23b5
Anthony Barbee <Anthony.Barbee@ibm.com>
2019-06-27 11:12:16 -0500

    MainContainer: Init structure
    Subject to change during implementation

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -2,3 +2,34 @@
  * Main Container of project
  */
 import React from 'react';
+
+export const className = 'main-container';
+
+class MainContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.fetchBlocks = this.fetchBlocks.bind(this);
+
+    this.state = {
+      blockInfo: {},
+    };
+  }
+
+  fetchBlocks() {
+    return '';
+  }
+
+  renderBlocks(info) {
+    return info;
+  }
+
+  render() {
+    return (
+      <div>
+        Test App
+      </div>
+    );
+  }
+}
+
+export default MainContainer;

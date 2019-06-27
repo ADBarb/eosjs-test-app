bb4bbb94dbce0fc704cff30ce628e47d4ffb0f5e
Anthony Barbee <Anthony.Barbee@ibm.com>
2019-06-27 13:30:39 -0500

    Add Button to main container

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -2,6 +2,7 @@
  * Main Container of project
  */
 import React from 'react';
+import { Button } from 'carbon-components-react';
 
 export const className = 'main-container';
 
@@ -16,17 +17,33 @@ class MainContainer extends React.Component {
   }
 
   fetchBlocks() {
+    console.log('Clicked fetchBlocks');
     return '';
   }
 
   renderBlocks(info) {
-    return info;
+    if (!info) {
+      return '';
+    }
+
+    return Object.keys(info).length !== 0
+      ? (
+        <div>
+          {info}
+        </div>
+      )
+      : '';
   }
 
   render() {
+    const { blockInfo } = this.state;
+
     return (
-      <div>
-        Test App
+      <div className={className}>
+        {this.renderBlocks(blockInfo)}
+        <Button onClick={this.fetchBlocks}>
+          Load
+        </Button>
       </div>
     );
   }

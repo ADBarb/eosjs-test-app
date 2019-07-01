05400fbb34ceaf30bbbee9e9a5a0257b7eb365bf
Anthony Barbee <Anthony.Barbee@ibm.com>
2019-07-01 10:59:20 -0500

    Blocks Action file - init + ActionTypes funcs

---
+++ src/actions/blocks.js
@@ -0,0 +1,13 @@
+/** 
+ * Action file for block related calls 
+ */
+import * as ActionTypes from './ActionTypes';
+
+export const loadingBlocks = isLoading => ({
+  type: ActionTypes.LOADING_BLOCKS,
+  payload: { isLoading },
+});
+export const blocks = blocks => ({
+  type: ActionTypes.BLOCKS_FETCHED,
+  payload: { blocks },
+});

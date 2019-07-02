67aafbedd4ecc15775e7fd65b9c8192e70f19a3b
Anthony Barbee <Anthony.Barbee@ibm.com>
2019-07-02 18:07:39 -0500

    Function to chain get_block x amount of times

--- src/actions/blocks.js
+++ src/actions/blocks.js
@@ -2,6 +2,9 @@
  * Action file for block related calls 
  */
 import * as ActionTypes from './ActionTypes';
+const { JsonRpc } = require('eosjs');
+const fetch = require('node-fetch');
+const rpc = new JsonRpc("https://api.eosnewyork.io", { fetch });
 
 export const loadingBlocks = isLoading => ({
   type: ActionTypes.LOADING_BLOCKS,
@@ -13,36 +16,30 @@ export const blocks = blocks => ({
 });
 
 /** 
- * get_info API call
- * Source: https://developers.eos.io/eosio-nodeos/reference#get_info
+ * Get info, then get header block, use that to loop through prev blocks and get their info
+ * get_info Source: https://developers.eos.io/eosio-nodeos/reference#get_info 
  */
-export const getInfo = () => (dispatch) => {
-  let data = null;
-  const xhr = new XMLHttpRequest();
-  xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === this.DONE) {
-      console.log('response', this.responseText);
+export const fetchBlocks = (num = 10) => (dispatch) => {
+  const blockList = [];
+  dispatch(loadingBlocks(true));
+  rpc.get_info().then((res) => {
+    let id = res.head_block_id;
+    let result = Promise.resolve();
+    for (let i = 0; i < num; i++) {
+      // eslint-disable-next-line
+      result = result.then(() => {
+        return rpc.get_block(id).then((res) => {
+          blockList.push(res);
+          id = res.previous;
+          if (blockList.length === num) {
+            dispatch(blocks(blockList));
+            dispatch(loadingBlocks(false));
+            return;
+          }
+        });
+      })
     }
+  }).catch((error) => {
+    console.log('Error in get_info: ', error);
   });
-
-  xhr.open("POST", "https://api.eosnewyork.io/v1/chain/get_info");
-  xhr.setRequestHeader("accept", "application/json");
-  xhr.send(data);
-};
-
-export const getBlock = (id) => (dispatch) => {
-  let data = null;
-  const xhr = new XMLHttpRequest();
-
-  xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === this.DONE) {
-      console.log(this.responseText);
-    }
-  });
-
-  xhr.open("POST", "http://localhost:8080/v1/chain/get_block");
-  xhr.setRequestHeader("accept", "application/json");
-  xhr.setRequestHeader("content-type", "application/json");
-
-  xhr.send(data);
-};
+}

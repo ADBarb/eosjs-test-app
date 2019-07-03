8ec8a2bbacd6cd6cf1c8f2c23d4097519dbe7008
Anthony Barbee <Anthony.Barbee@ibm.com>
2019-07-03 00:01:59 -0500

    Add get_info fixture

--- src/fixtures/blocks.js
+++ src/fixtures/blocks.js
@@ -15,3 +15,26 @@ module.exports.testBlockData = {
   "transaction_mroot": "0000000000000000000000000000000000000000000000000000000000000000",
   "transactions": [],
 };
+
+module.exports.testGetInfo = {
+  "account_name": "testacc",
+  "head_block_num": 1079,
+  "head_block_id": "00000001bcf2f448225d099685f14da76803028926af04d2607eafcf609c265c",
+  "head_block_time": "2018-11-10T00:45:53.500",
+  "privileged": false,
+  "last_code_update": "1970-01-01T00:00:00.000",
+  "created": "2018-11-10T00:37:05.000",
+  "ram_quota": -1,
+  "net_weight": -1,
+  "cpu_weight": -1,
+  "net_limit": { "used": -1, "available": -1, "max": -1 },
+  "cpu_limit": { "used": -1, "available": -1, "max": -1 },
+  "ram_usage": 2724,
+  "permissions":
+   [ { "perm_name": "active", "parent": "owner", "required_auth": [] },
+     { "perm_name": "owner", "parent": "", "required_auth": [] } ],
+  "total_resources": null,
+  "self_delegated_bandwidth": null,
+  "refund_request": null,
+  "voter_info": null,
+};
